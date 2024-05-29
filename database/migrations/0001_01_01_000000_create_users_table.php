66012bca75fda91872e108ca756a72466f9d27a8
WILSON PAREDES <wilsonparedes87@gmail.com>
2024-05-29 11:10:53 -0500

    Update migrate user

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,13 +13,13 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('ic');
+            $table->string('ci')->unique();
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
-            $table->enum('role',['docente' => 1, 'estudiante' => 2]);
+            $table->enum('role',['docente' => 1, 'estudiante' => 2])->default('2');
             $table->timestamps();
         });
 
