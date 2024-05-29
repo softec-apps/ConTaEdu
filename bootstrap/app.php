0b08d0994256042b10721bedfb6b1cc88bca4228
LGsus1224 <gonzalezjesusetluna1@gmail.com>
2024-05-29 11:20:42 -0500

    Middleware and protection routes groups by rol

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -11,7 +11,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'checkPermission' => \App\Http\Middleware\CheckPermission::class,
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
