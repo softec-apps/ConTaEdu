fb9fc3729fce381a656b334bdae7803f81a9c401
LGsus1224 <gonzalezjesusetluna1@gmail.com>
2024-05-30 09:41:23 -0500

    Auth views

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,47 +1,32 @@
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<x-auth-layout>
+	<div class="app-auth-body mx-auto">
+		<div class="app-auth-branding mb-4"><a class="d-flex justify-content-center app-logo"
+				href="{{ route('login') }}">
+				<x-application-logo /></a></div>
+		<h2 class="auth-heading text-center mb-5">Iniciar sesión en ConTaEdu</h2>
+		<div class="auth-form-container text-start">
+			<form method="POST" action="{{ route('login') }}" class="auth-form login-form">
+				@csrf
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+				<div class="mb-3">
+					<x-input-label for="email" class="sr-only" :value="__('Email')" />
+					<x-text-input id="email" class="form-control signin-email" type="email" name="email" :value="old('email')"
+						required placeholder="Email" autofocus autocomplete="username" />
+					<x-input-error :messages="$errors->get('email')" class="mt-2" />
+				</div>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
+				<div class="mb-3">
+					<x-input-label for="password" class="sr-only" :value="__('Contraseña')" />
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Contraseña')" />
+					<x-text-input id="password" class="form-control signin-password" type="password" name="password" required
+						autocomplete="current-password" placeholder="Contraseña"/>
+					<x-input-error :messages="$errors->get('password')" class="mt-2" />
+				</div>
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                            type="password"
-                            name="password"
-                            required autocomplete="current-password" />
-
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
-
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                    {{ __('Forgot your password?') }}
-                </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
+				<div class="text-center w-100">
+					<button type="submit" class="btn app-btn-primary w-100 theme-btn mx-auto">Iniciar sesión</button>
+				</div>
+			</form>
+		</div><!--//auth-form-container-->
+	</div><!--//auth-body-->
+</x-auth-layout>
