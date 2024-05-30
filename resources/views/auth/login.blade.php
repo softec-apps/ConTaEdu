108bdd1108c935aa0534881460be55ba053226eb
LGsus1224 <gonzalezjesusetluna1@gmail.com>
2024-05-30 11:33:22 -0500

    Dashboard template

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,32 +1,39 @@
-<x-auth-layout>
-	<div class="app-auth-body mx-auto">
-		<div class="app-auth-branding mb-4"><a class="d-flex justify-content-center app-logo"
-				href="{{ route('login') }}">
-				<x-application-logo /></a></div>
-		<h2 class="auth-heading text-center mb-5">Iniciar sesión en ConTaEdu</h2>
-		<div class="auth-form-container text-start">
-			<form method="POST" action="{{ route('login') }}" class="auth-form login-form">
-				@csrf
+<x-app-layout>
+	<x-slot name="title">Login</x-slot>
 
-				<div class="mb-3">
-					<x-input-label for="email" class="sr-only" :value="__('Email')" />
-					<x-text-input id="email" class="form-control signin-email" type="email" name="email" :value="old('email')"
-						required placeholder="Email" autofocus autocomplete="username" />
-					<x-input-error :messages="$errors->get('email')" class="mt-2" />
-				</div>
+	@section('main')
+		<x-layouts.auth>
+			<div class="app-auth-body mx-auto">
+				<div class="app-auth-branding mb-4"><a class="d-flex justify-content-center app-logo"
+						href="{{ route('login') }}">
+						<x-application-logo /></a></div>
+				<h2 class="auth-heading text-center mb-5">Iniciar sesión en ConTaEdu</h2>
+				<div class="auth-form-container text-start">
+					<form method="POST" action="{{ route('login') }}" class="auth-form login-form">
+						@csrf
 
-				<div class="mb-3">
-					<x-input-label for="password" class="sr-only" :value="__('Contraseña')" />
+						<div class="mb-3">
+							<x-input-label for="email" class="sr-only" :value="__('Email')" />
+							<x-text-input id="email" class="form-control signin-email" type="email" name="email"
+								:value="old('email')" required placeholder="Email" autofocus autocomplete="username" />
+							<x-input-error :messages="$errors->get('email')" class="mt-2" />
+						</div>
 
-					<x-text-input id="password" class="form-control signin-password" type="password" name="password" required
-						autocomplete="current-password" placeholder="Contraseña"/>
-					<x-input-error :messages="$errors->get('password')" class="mt-2" />
-				</div>
+						<div class="mb-3">
+							<x-input-label for="password" class="sr-only" :value="__('Contraseña')" />
 
-				<div class="text-center w-100">
-					<button type="submit" class="btn app-btn-primary w-100 theme-btn mx-auto">Iniciar sesión</button>
-				</div>
-			</form>
-		</div><!--//auth-form-container-->
-	</div><!--//auth-body-->
-</x-auth-layout>
+							<x-text-input id="password" class="form-control signin-password" type="password" name="password"
+								required autocomplete="current-password" placeholder="Contraseña" />
+							<x-input-error :messages="$errors->get('password')" class="mt-2" />
+						</div>
+
+						<div class="text-center w-100">
+							<button type="submit" class="btn app-btn-primary w-100 theme-btn mx-auto">Iniciar
+								sesión</button>
+						</div>
+					</form>
+				</div><!--//auth-form-container-->
+			</div><!--//auth-body-->
+		</x-layouts.auth>
+	@endsection
+</x-app-layout>
---
+++ resources/views/components/layouts/auth.blade.php
@@ -0,0 +1,24 @@
+<main class="app app-login p-0">
+    <div class="row g-0 app-auth-wrapper">
+        <div class="col-12 col-md-7 col-lg-6 auth-main-col text-center p-5">
+            <div class="d-flex flex-column align-content-end">
+                {{ $slot }}
+                <x-app-footer></x-app-footer>
+            </div><!--//flex-column-->
+        </div><!--//auth-main-col-->
+        <style>
+            .new-auth-background-holder {
+                background: url("{{ asset('logo.png') }}") no-repeat center center;
+                background-size: cover;
+                height: 100vh;
+                min-height: 100%
+            }
+        </style>
+        <div class="col-12 col-md-5 col-lg-6 h-100 auth-background-col">
+            <div class="new-auth-background-holder">
+            </div>
+            <div class="auth-background-mask"></div>
+        </div><!--//auth-background-col-->
+
+    </div><!--//row-->
+</main>
