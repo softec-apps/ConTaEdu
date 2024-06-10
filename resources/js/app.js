72f782a1751870e65c413e5abccefe50c84cb0ae
Alexander-Luna <paulluna99@gmail.com>
2024-06-10 11:12:53 -0500

    JQUERY

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,10 @@ import $ from 'jquery';
 window.$ = window.jQuery = $;
 
 import * as bootstrap from 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/js/all.min.js';
 
 import 'jszip';
 import 'pdfmake';
