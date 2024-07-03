cb0664766794d9b4c576cd3cbcaebb53c8c13b1a
LGsus1224 <gonzalezjesusetluna1@gmail.com>
2024-07-03 12:38:51 -0500

    Primary btn text color changed to white by default

--- resources/views/components/primary-button.blade.php
+++ resources/views/components/primary-button.blade.php
@@ -9,6 +9,6 @@
         disabled: false,
     }" x-bind:disabled="loading || disabled"
     x-html="loading ? '<span class=\'spinner-border spinner-border-sm\' role=\'status\' aria-hidden=\'true\'></span> Buscando...' : text"
-    {{ $attributes->merge(['type' => 'button', 'class' => $custom ? 'btn' : 'btn btn-primary']) }}>
+    {{ $attributes->merge(['type' => 'button', 'class' => $custom ? 'btn' : 'btn btn-primary text-white']) }}>
     {{ $slot }}
 </button>
