96b6f846429342f5a8204605c67b798379a8e710
LGsus1224 <gonzalezjesusetluna1@gmail.com>
2024-07-04 15:49:32 -0500

    Vista de ejercicios pendientes y enviados

--- resources/views/estudiante/dashboard.blade.php
+++ resources/views/estudiante/dashboard.blade.php
@@ -1,7 +1,10 @@
 <x-app-layout>
     <x-slot:title>Tablero</x-slot>
 
-        @section('main')
+    @section('main')
+    <!-- Join to exercise -->
+    <x-estudiante.joinToExercise />
+
         <x-layouts.dashboard>
             <div class="app-wrapper">
                 <div class="app-content pt-3 p-md-3 p-lg-4">
@@ -24,7 +27,8 @@
                                         </div>
                                     </div>
                                     <!--//app-card-body-->
-                                    <a class="app-card-link-mask" href="#" data-bs-toggle="modal" data-bs-target="#accessToExercise"></a>
+                                    <!-- <a class="app-card-link-mask" href="#" data-bs-toggle="modal" data-bs-target="#accessToExercise"></a> -->
+                                    <a class="app-card-link-mask" href="#" @click="showAccessModal = !showAccessModal"></a>
                                 </div>
                                 <!--//app-card-->
                             </div>
@@ -76,12 +80,7 @@
                                     <!--//app-card-header-->
                                     <div class="app-card-body p-3 p-lg-4">
                                         <div id="active-courses">
-                                            <x-loading-items :items="$sent_graded_exercises">
-                                                <x-slot:items_code>
-                                                    @foreach ($sent_graded_exercises as $item)
-                                                        <x-card-item :exercise="$item" :calificado="true"></x-card-item>
-                                                    @endforeach
-                                                </x-slot:items_code>
+                                            <x-loading-items :items="$sent_graded_exercises" :graded="true">
                                                 <div class="d-flex justify-content-center align-items-center">
                                                     <a href="{{ route('estudiante.sent_graded_exercises') }}" class="btn btn-secondary">Ver todo</a>
                                                 </div>
@@ -96,56 +95,6 @@
                         </div>
                         <!--//row-->
                     </div>
-
-                    <x-modal :name="'accessToExercise'" :size="__('sm')" :show="false" :title="__('Nuevo ejercicio')" :form="true">
-                        <div class="mb-3">
-                            <x-input-label for="access-code" :value="__('Código de acceso')"/>
-                            <x-text-input name="access-code" id="access-code" placeholder="XXXXXX" autocomplete="off" required minlength="6" maxlength="6"/>
-                            <x-input-error :messages="null" id="searchExerciseError"/>
-                        </div>
-                        <small class="text-info">El código de ejercicio está formado con 6 dígitos. Solicita a tu docente el código.</small>
-
-                        <x-slot:footer>
-                            <x-primary-button :custom="true" class="btn-secondary" data-bs-dismiss="modal" id="cancelSearch-btn">Cancelar</x-primary-button>
-                            <x-primary-button form="accessToExerciseForm" type="submit" id="searchExercise-btn">Buscar</x-primary-button>
-                        </x-slot>
-                    </x-modal>
-
-                    @php
-                        $formAction = route('estudiante.join_exercise');
-                    @endphp
-                    <x-modal :name="'exerciseFound'" :show="false" :title="__('Unirse al ejercicio')"
-                    :form="true" :form_action="$formAction" :submit_text="__('Unirse')">
-                        <div class="mb-3">
-                            <input type="hidden" name="access-code" id="access-code" value="">
-                            <input type="hidden" name="exercise-id" id="exercise-id" value="">
-                            <div class="row my-3">
-                                <div class="col-12">
-                                    <h5 id="exerciseTitle">Ejercicio</h5>
-                                    <details class="mb-3">
-                                        <div class="row">
-                                            <small class="col-12">Docente: <span id="exerciseTeacher" class="badge bg-info"></span></small>
-                                            <small class="col-12">Código: <span id="exerciseAccessCode" class="badge bg-info">XXXXXX</span></small>
-                                        </div>
-                                    </details>
-                                </div>
-                                <div class="col-5">
-                                    <div class="app-card app-card-doc shadow-sm h-100">
-                                        <div class="app-card-thumb-holder p-3">
-                                            <span class="icon-holder text-info">
-                                                <i class="fa-solid fa-file-invoice-dollar"></i>
-                                            </span>
-
-                                            <a class="app-card-link-mask" href="#"></a>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="col-7">
-                                    <p id="exerciseDescription">Descripción</p>
-                                </div>
-                            </div>
-                        </div>
-                    </x-modal>
                     <!--//container-fluid-->
                 </div>
                 <!--//app-content-->
@@ -153,64 +102,5 @@
             </div>
             <!--//app-wrapper-->
         </x-layouts.dashboard>
-    @endsection
-
-    @section('scripts')
-    <script>
-        $(document).ready(function() {
-            function searchExercise(formData) {
-                // Realizar la consulta de ejercicio
-                return $.ajax({
-                    type:'POST',
-                    url: '{{ route('estudiante.search_exercise') }}',
-                    headers: {
-                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-                    },
-                    data: formData,
-                });
-            }
-
-            $('#accessToExerciseForm').submit(function(e) {
-                e.preventDefault();
-                const modal1 = $('#accessToExercise')[0];
-                const modal2 = $('#exerciseFound')[0];
-                const searchExerciseBtn = $('#searchExercise-btn')[0];
-                const cancelSearchBtn = $('#cancelSearch-btn')[0];
-                const searchExerciseError = $('#searchExerciseError')[0];
-                // Deshabilitar botones
-                Alpine.$data(searchExerciseBtn).loading = true;
-                Alpine.$data(cancelSearchBtn).disabled = true;
-
-                // Limpiar errores del input
-                Alpine.$data(searchExerciseError).clearMessages();
-
-                // Simula una operación (p. ej., una llamada a una API)
-                searchExercise($(this).serialize()).then(function (response) {
-                    if (response.success) {
-                        Alpine.$data(searchExerciseBtn).loading = false;
-                        Alpine.$data(cancelSearchBtn).disabled = false;
-                        Alpine.$data(modal1).show = false;
-                        Alpine.$data(modal2).show = true;
-
-                        // Cargar los datos del ejercicio
-                        $(modal2).find('#access-code').val(response.data.access_code);
-                        $(modal2).find('#exercise-id').val(response.data.id);
-                        $(modal2).find('#exerciseName').text(response.data.name);
-                        $(modal2).find('#exerciseAccessCode').text(response.data.access_code);
-                        $(modal2).find('#exerciseDescription').text(response.data.desc);
-                    } else {
-                        Alpine.$data(searchExerciseError).setMessages(['Ocurrió un error inesperado']);
-                        Alpine.$data(searchExerciseBtn).loading = false;
-                        Alpine.$data(cancelSearchBtn).disabled = false;
-                    }
-                }).catch(function (error) {
-                    // En caso de un error inesperado, también habilitar los botones y mostrar la alerta
-                    Alpine.$data(searchExerciseError).setMessages(['No se pudo encontrar ningún ejercicio']);
-                    Alpine.$data(searchExerciseBtn).loading = false;
-                    Alpine.$data(cancelSearchBtn).disabled = false;
-                });
-            })
-        })
-    </script>
     @endsection
 </x-app-layout>
---
+++ resources/views/estudiante/pending_exercises.blade.php
@@ -0,0 +1,67 @@
+<x-app-layout>
+  <x-slot:title>Pendientes</x-slot>
+  @section('main')
+  <!-- Join to exercise -->
+  <x-estudiante.joinToExercise />
+    <x-layouts.dashboard>
+      <div class="app-wrapper">
+        <div class="app-content pt-3 p-md-3 p-lg-4">
+          <div class="container-xl">
+            <div class="row g-3 mb-4 align-items-center justify-content-between">
+              <div class="col-auto">
+                <h1 class="app-page-title mb-0">Ejercicios Pendientes</h1>
+              </div>
+              <div class="col-auto">
+                <div class="page-utilities">
+                  <div
+                    class="row g-2 justify-content-start justify-content-md-end align-items-center">
+                    <div class="col-auto">
+                      <form class="docs-search-form row gx-1 align-items-center"
+                        action="{{ route('exercise.search') }}" method="GET">
+                        <div class="col-auto">
+                          <input type="text" id="search-docs" name="searchdocs"
+                            class="form-control search-docs"
+                            placeholder="Ingrese el título">
+                        </div>
+                        <div class="col-auto">
+                          <button type="submit"
+                            class="btn app-btn-secondary">Buscar</button>
+                        </div>
+                      </form>
+                    </div>
+                    <div class="col-auto">
+                      <a class="btn app-btn-primary" href="#"
+                        data-bs-toggle="modal" data-bs-target="#modalRegister">
+                        <svg width="1em" height="1em" viewBox="0 0 16 16"
+                          class="bi bi-file-earmark-plus me-2" fill="currentColor"
+                          xmlns="http://www.w3.org/2000/svg">
+                          <path
+                            d="M8 6a.5.5 0 0 1 .5.5V8h1.5a.5.5 0 0 1 0 1H8.5v1.5a.5.5 0 0 1-1 0V9H6a.5.5 0 0 1 0-1h1.5V6.5A.5.5 0 0 1 8 6z" />
+                          <path
+                            d="M14 4.5V14a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h5.5L14 4.5zM13.5 4H9a1 1 0 0 1-1-1V.5L13.5 4z" />
+                        </svg>
+                        Crear Ejercicio
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div class="row g-4">
+              @foreach ($exercises as $exercise)
+                <x-card-item :exercise="$exercise"></x-card-item>
+              @endforeach
+
+              <nav class="app-pagination mt-5">
+                <ul class="pagination justify-content-center">
+                  {{ $exercises->links('vendor.pagination.bootstrap-4') }}
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+    </x-layouts.dashboard>
+  @endsection
+</x-app-layout>
+<x-modal-register />
