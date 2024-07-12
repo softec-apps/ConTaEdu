68879ba7011cd71e36b9ed61bc73b81c6a867c73
LGsus1224 <gonzalezjesusetluna1@gmail.com>
2024-07-12 12:10:55 -0500

    Asientos contables estudiante completo

--- resources/views/components/estudiante/detalle-asiento-modal.blade.php
+++ resources/views/components/estudiante/detalle-asiento-modal.blade.php
@@ -1,29 +1,60 @@
-@props(['index'])
+@props([
+    'index',
+    'update' => false
+])
 
-<div class="row cuenta-row mb-3 d-flex justify-content-between align-items-center">
+@php
+    // Clases de las filas
+    $cuentaRowClass = 'cuenta-row';
+    $cuentaRowClass = $update ? 'update-' . $cuentaRowClass : $cuentaRowClass;
+
+    $accountTypeClass = 'account-type';
+    $accountTypeClass = $update ? 'update-' . $accountTypeClass : $accountTypeClass;
+
+    $transactionTypeClass = 'transaction-type';
+    $transactionTypeClass = $update ? 'update-' . $transactionTypeClass : $transactionTypeClass;
+
+    $amountClass = 'amount';
+    $amountClass = $update ? 'update-' . $amountClass : $amountClass;
+
+    $removeCuentaClass = 'remove-cuenta';
+    $removeCuentaClass = $update ? 'update-' . $removeCuentaClass : $removeCuentaClass;
+
+    // IDs de los inputs
+    $accountIdInputID = 'cuentas_' . $index . '_account_id';
+    $accountIdInputID = $update ? 'update-' . $accountIdInputID : $accountIdInputID;
+
+    $transactionTypeInputID = 'cuentas_' . $index . '_type';
+    $transactionTypeInputID = $update ? 'update-' . $transactionTypeInputID : $transactionTypeInputID;
+
+    $amountInputID = 'cuentas_' . $index . '_amount';
+    $amountInputID = $update ? 'update-' . $amountInputID : $amountInputID;
+@endphp
+
+<div class="row {{ $cuentaRowClass }} mb-3 d-flex justify-content-between align-items-center">
     <div class="col-11">
         <div class="row">
             <div class="form-floating col-lg-6">
-                <select class="form-control account-select-{{ $index }}" id="cuentas[{{ $index }}][account_id]"
+                <select class="form-control {{ $accountTypeClass }}" id="{{ $accountIdInputID }}"
                     name="cuentas[{{ $index }}][account_id]" required style="width: 100%"></select>
-                <label for="cuentas[{{ $index }}][account_id]">Cuenta</label>
+                <label for="{{ $accountIdInputID }}">Cuenta</label>
             </div>
             <div class="form-floating col-lg-3">
-                <select class="form-select transaction-type" id="cuentas[{{ $index }}][type]"
+                <select class="form-select {{ $transactionTypeClass }}" id="{{ $transactionTypeInputID }}"
                     name="cuentas[{{ $index }}][type]" required>
                     <option value="debe">Debe</option>
                     <option value="haber">Haber</option>
                 </select>
-                <label for="cuentas[{{ $index }}][type]">Tipo de Transacción</label>
+                <label for="{{ $transactionTypeInputID }}">Tipo de Transacción</label>
             </div>
             <div class="form-floating col-lg-3">
-                <input type="number" class="form-control amount" id="cuentas[{{ $index }}][amount]"
+                <input type="number" class="form-control {{ $amountClass }}" id="{{ $amountInputID }}"
                     name="cuentas[{{ $index }}][amount]" required step="0.01">
-                <label for="cuentas[{{ $index }}][amount]">Monto</label>
+                <label for="{{ $amountInputID }}">Monto</label>
             </div>
         </div>
     </div>
     <div class="col-1 h-100">
-        <x-primary-button :custom="true" class="btn btn-block btn-danger text-white h-100 remove-cuenta">X</x-primary-button>
+        <x-primary-button :custom="true" class="btn btn-block btn-danger text-white h-100 {{ $removeCuentaClass }}">X</x-primary-button>
     </div>
 </div>
