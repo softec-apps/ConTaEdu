21284e07371e632d5f78c4a2fb6a9d9a5729e4d9
LGsus1224 <gonzalezjesusetluna1@gmail.com>
2024-07-17 12:18:34 -0500

    Libro diario y libro mayor en vista de estudiante y docente

--- resources/views/estudiante/libro-mayor.blade.php
+++ resources/views/estudiante/libro-mayor.blade.php
@@ -5,7 +5,7 @@
             <div class="app-content pt-3 p-md-3 p-lg-4">
                 <div class="container-xl">
                     <div class="d-flex justify-content-between align-items-center mb-2">
-                        <h1 class="app-page-title">Libro Diario - {{ $exercise->titulo }}</h1>
+                        <h1 class="app-page-title">Libro Mayor - {{ $exercise->titulo }}</h1>
                         @if (\Auth::user()->role == 3)
                             <!-- Mostrar botón para regresar -->
                             <a href="{{ route('estudiante.see_exercise', ['id' => $exercise->id]) }}" class="btn btn-primary text-white">Volver</a>
@@ -20,9 +20,9 @@
                     <div class="d-flex justify-content-end mb-3">
                         <x-primary-button :custom="true" class="btn btn-danger text-white"
                             onclick="printJS({
-                                printable: 'libro-diario', type: 'html',
+                                printable: 'libro-mayor', type: 'html',
                                 css: ['{{ asset('../resources/css/app.css') }}'],
-                                documentTitle: 'Libro Diario - {{ $exercise->titulo }}'
+                                documentTitle: 'Libro Mayor - {{ $exercise->titulo }}'
                             })">
                             <i class="fa-solid fa-file-pdf"></i> Imprimir
                         </x-primary-button>
@@ -30,7 +30,7 @@
 
                     <div class="card">
                         <div class="card-body">
-                            <div class="table-responsive" id="libro-diario">
+                            <div class="table-responsive" id="libro-mayor">
                                 <div class="only-print">
                                     <div class="d-flex justify-content-end">
                                         <p><strong>Estudiante:</strong> {{ $exercise->asignaciones->estudiante->name }}</p>
@@ -38,25 +38,27 @@
                                 </div>
                                 <table class="table table-hover">
                                     <tbody>
-                                        @foreach($libroDiario as $asiento)
-                                            <tr class="table-active">
-                                                <td colspan="5"><strong>{{ $asiento['fecha'] }} - {{ $asiento['concepto'] }}</strong></td>
+                                        @foreach($cuentas as $cuenta)
+                                            <tr>
+                                                <td colspan="5" class="pt-4"><strong>{{ $cuenta['cuenta'] }}</strong></td>
                                             </tr>
-                                            @foreach($asiento['detalles'] as $detalle)
+                                            <tr class="table-active">
+                                                <th>Fecha</th>
+                                                <th>Concepto</th>
+                                                <th>Debe</th>
+                                                <th>Haber</th>
+                                                <th>Saldo</th>
+                                            </tr>
+                                            @foreach($cuenta['movimientos'] as $movimiento)
                                                 <tr>
-                                                    <td></td>
-                                                    <td>{{ $detalle['cuenta'] }}</td>
-                                                    <td>{{ $detalle['descripcion'] }}</td>
-                                                    <td>$ {{ number_format($detalle['debe'], 2) }}</td>
-                                                    <td>$ {{ number_format($detalle['haber'], 2) }}</td>
+                                                    <td>{{ $movimiento['fecha'] }}</td>
+                                                    <td>{{ $movimiento['concepto'] }}</td>
+                                                    <td>$ {{ number_format($movimiento['debe'], 2) }}</td>
+                                                    <td>$ {{ number_format($movimiento['haber'], 2) }}</td>
+                                                    <td>$ {{ number_format($movimiento['debe'] - $movimiento['haber'], 2) }}</td>
                                                 </tr>
                                             @endforeach
                                         @endforeach
-                                        <tr class="table-active">
-                                            <td colspan="3"><strong>Totales</strong></td>
-                                            <td><strong>$ {{ number_format($totalDebe, 2) }}</strong></td>
-                                            <td><strong>$ {{ number_format($totalHaber, 2) }}</strong></td>
-                                        </tr>
                                     </tbody>
                                 </table>
                             </div>
