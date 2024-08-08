1291eb0dd42a22ae1e924574cb41a3c3052462fc
Alexander-Luna <paulluna99@gmail.com>
2024-08-08 17:14:52 -0500

    Reset Pass1

--- resources/views/auth/reset-password-form.blade.php
+++ resources/views/auth/reset-password-form.blade.php
@@ -11,25 +11,27 @@
                 </div>
                 <h2 class="auth-heading text-center mb-5">Restablecer Contraseña</h2>
                 <div class="auth-form-container text-start">
-                    <form method="POST" action="{{ route('password') }}">
+                    <form id="reset-password-form">
                         @csrf
                         <input type="hidden" name="token" id="token">
                         <input type="hidden" name="email" id="email">
-                    
+
                         <div class="mb-3">
                             <x-input-label for="password" :value="__('Nueva Contraseña')" />
                             <x-text-input id="password" class="form-control" type="password" name="password" required />
                             <x-input-error :messages="$errors->get('password')" class="mt-2" />
                         </div>
-                    
+
                         <div class="mb-3">
                             <x-input-label for="password_confirmation" :value="__('Confirmar Contraseña')" />
-                            <x-text-input id="password_confirmation" class="form-control" type="password" name="password_confirmation" required />
+                            <x-text-input id="password_confirmation" class="form-control" type="password"
+                                name="password_confirmation" required />
                             <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2" />
                         </div>
-                    
+
                         <div class="text-center">
-                            <button type="submit" class="btn app-btn-primary">Restablecer Contraseña</button>
+                            <button type="button" id="submit-button" class="btn app-btn-primary">Restablecer
+                                Contraseña</button>
                         </div>
                     </form>
                     <script>
@@ -43,8 +45,44 @@
                             document.getElementById('token').value = token;
                             document.getElementById('email').value = email;
                         });
+
+                        document.getElementById('submit-button').addEventListener('click', function() {
+                            const form = document.getElementById('reset-password-form');
+                            const formData = new FormData(form);
+
+                            fetch('{{ route('password') }}', {
+                                    method: 'POST',
+                                    body: formData,
+                                })
+                                .then(response => response.json())
+                                .then(data => {
+                                    window.location.reload();
+                                    if (!data.success) {
+                                        Swal.fire({
+                                            title: 'Error',
+                                            text: data.errors.email ? data.errors.email[0] :
+                                                'Hubo un error al restablecer la contraseña.',
+                                            icon: 'error',
+                                            toast: true,
+                                            position: 'top-end',
+                                            showConfirmButton: false,
+                                            timer: 3000
+                                        });
+                                    }
+                                })
+                                .catch(error => {
+                                    Swal.fire({
+                                        title: 'Error',
+                                        text: 'Hubo un error al enviar el formulario. Inténtalo de nuevo.',
+                                        icon: 'error',
+                                        toast: true,
+                                        position: 'top-end',
+                                        showConfirmButton: false,
+                                        timer: 3000
+                                    });
+                                });
+                        });
                     </script>
-                    
                 </div><!--//auth-form-container-->
             </div><!--//auth-body-->
         </x-layouts.auth>
--- resources/views/components/modal-resetpass.blade.php
+++ resources/views/components/modal-resetpass.blade.php
@@ -1,58 +1,90 @@
 <!-- Password Reset Modal -->
 <div class="modal fade" id="passwordResetModal" tabindex="-1" aria-labelledby="passwordResetModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-      <div class="modal-content">
-          <div class="modal-header">
-              <h5 class="modal-title" id="passwordResetModalLabel">Restablecer Contraseña</h5>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-          </div>
-          <div class="modal-body">
-              <form id="passwordResetForm" method="POST" action="{{ route('password.email') }}">
-                  @csrf
-                  <div class="mb-3">
-                      <x-input-label for="reset-email" :value="__('Email')" />
-                      <x-text-input id="reset-email" class="form-control" type="email" name="email" required
-                          placeholder="Introduce tu email" />
-                      <x-input-error :messages="$errors->get('email')" class="mt-2" />
-                  </div>
-                  <div class="text-center">
-                      <button type="button" class="btn app-btn-primary" id="reset-password-btn">Enviar enlace de restablecimiento</button>
-                  </div>
-              </form>
-          </div>
-      </div>
-  </div>
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="passwordResetModalLabel">Restablecer Contraseña</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <form id="passwordResetForm" method="POST" action="{{ route('password.email') }}">
+                    @csrf
+                    <div class="mb-3">
+                        <x-input-label for="reset-email" :value="__('Email')" />
+                        <x-text-input id="reset-email" class="form-control" type="email" name="email" required
+                            placeholder="Introduce tu email" />
+                        <x-input-error :messages="$errors->get('email')" class="mt-2" />
+                    </div>
+                    <div class="text-center">
+                        <button type="button" class="btn app-btn-primary" id="reset-password-btn">Enviar enlace de
+                            restablecimiento</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </div>
 <script>
-  document.addEventListener('DOMContentLoaded', function () {
-      const resetPasswordBtn = document.getElementById('reset-password-btn');
-      const passwordResetForm = document.getElementById('passwordResetForm');
-      
-      resetPasswordBtn.addEventListener('click', function (event) {
-          event.preventDefault(); // Previene el comportamiento por defecto del formulario
-          const formData = new FormData(passwordResetForm);
-          fetch(passwordResetForm.action, {
-              method: 'POST',
-              body: formData,
-              headers: {
-                  'X-Requested-With': 'XMLHttpRequest',
-                  'X-CSRF-TOKEN': formData.get('_token') // Agrega el token CSRF para seguridad
-              }
-          })
-          .then(response => response.json())
-          .then(data => {
-              if (data.success) {
-                  alert('Enlace de restablecimiento enviado a tu correo electrónico.');
-                  const modal = bootstrap.Modal.getInstance(document.getElementById('passwordResetModal'));
-                  modal.hide();
-              } else {
-                  alert('Hubo un error al enviar el enlace. Inténtalo de nuevo.');
-                  console.log(data.errors); // Muestra los errores en la consola para depuración
-              }
-          })
-          .catch(error => {
-              console.error('Error:', error);
-          });
-      });
-  });
-  </script>
+    document.addEventListener('DOMContentLoaded', function() {
+
+        const resetPasswordBtn = document.getElementById('reset-password-btn');
+        const passwordResetForm = document.getElementById('passwordResetForm');
+
+        resetPasswordBtn.addEventListener('click', function(event) {
+            event.preventDefault(); // Previene el comportamiento por defecto del formulario
+            const formData = new FormData(passwordResetForm);
+            fetch(passwordResetForm.action, {
+                    method: 'POST',
+                    body: formData,
+                    headers: {
+                        'X-Requested-With': 'XMLHttpRequest',
+                        'X-CSRF-TOKEN': formData.get('_token')
+                    }
+                })
+                .then(response => response.json())
+                .then(data => {
+                    if (data.success) {
+
+                        Swal.fire({
+                            title: 'Actualización exitosa',
+                            text: 'Enlace de restablecimiento enviado a tu correo electrónico.',
+                            icon: 'success',
+                            toast: true,
+                            position: 'top-end',
+                            showConfirmButton: false,
+                            timer: 3000
+                        });
+
+                        const modal = bootstrap.Modal.getInstance(document.getElementById(
+                            'passwordResetModal'));
+                        modal.hide();
+                    } else {
+
+                        Swal.fire({
+                            title: 'Error',
+                            text: 'Hubo un error al enviar el enlace. Inténtalo de nuevo.',
+                            icon: 'error',
+                            toast: true,
+                            position: 'top-end',
+                            showConfirmButton: false,
+                            timer: 3000
+                        });
+                        console.log(data
+                            .errors); // Muestra los errores en la consola para depuración
+                    }
+                })
+                .catch(error => {
+                    Swal.fire({
+                        title: 'Error',
+                        text: 'Hubo un error al enviar el enlace. Inténtalo de nuevo.',
+                        icon: 'error',
+                        toast: true,
+                        position: 'top-end',
+                        showConfirmButton: false,
+                        timer: 3000
+                    });
+                    console.error('Error:', error);
+                });
+        });
+    });
+</script>
